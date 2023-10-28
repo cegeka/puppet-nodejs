cegeka/puppet-nodejs

commit 160ea93dbc6f6633d01f0a078541bbad21ad93ba

ensure npm to present by default on EL
nodesource started adding "provides: npm" to their nodejs packages,
which makes our code uninstall the nodejs package again

manifests/params.pp
@@ -38,7 +38,7 @@ class nodejs::params {
       $nodejs_dev_package_name   = 'nodejs-devel'
       $nodejs_dev_package_ensure = 'absent'
       $nodejs_package_name       = 'nodejs'
-      $npm_package_ensure        = 'absent'
+      $npm_package_ensure        = 'present'
       $npm_package_name          = 'npm'
       $npm_path                  = '/usr/bin/npm'
       $repo_class                = 'nodejs::repo::nodesource'

commit 070a73bea1fd393d0877cc64580dfed88c5fc390

only ensure real npm packages
nodesource's `nodejs` packages have `Provides: npm` which means that when
we try to `ensure => absent` the `npm` package, it really removes the
`nodejs` package we want.

by setting `allow_virtual => false` when using nodesource packages,
we ensure that we only remove real packages.

Fixes: 160ea93dbc6f6633d01f0a078541bbad21ad93ba

manifests/install.pp
@@ -45,9 +45,17 @@ class nodejs::install {
 
   # npm
   if $nodejs::npm_package_name and $nodejs::npm_package_name != false {
+    # the nodesource nodejs packages provide "npm" which makes Puppet
+    # try to uninstall them again
+    if $nodejs::npm_package_ensure == absent and $nodejs::manage_package_repo == true and $nodejs::repo_class == 'nodejs::repo::nodesource' {
+      $allow_virtual = false
+    } else {
+      $allow_virtual = undef
+    }
     package { $nodejs::npm_package_name:
-      ensure => $nodejs::npm_package_ensure,
-      tag    => 'nodesource_repo',
+      ensure        => $nodejs::npm_package_ensure,
+      allow_virtual => $allow_virtual,
+      tag           => 'nodesource_repo',
     }
   }
 

manifests/params.pp
@@ -41,7 +41,7 @@ class nodejs::params {
       $nodejs_dev_package_name   = 'nodejs-devel'
       $nodejs_dev_package_ensure = 'absent'
       $nodejs_package_name       = 'nodejs'
-      $npm_package_ensure        = 'present'
+      $npm_package_ensure        = 'absent'
       $npm_package_name          = 'npm'
       $npm_path                  = '/usr/bin/npm'
       $repo_class                = 'nodejs::repo::nodesource'